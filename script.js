8e848aea56c666782e4cdeaf4d7d4b0ecae9eaf8
Vamshi1616 <150407142+Vamshi1616@users.noreply.github.com>
2024-07-25 21:21:33 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.product-item button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Item added to cart!');
+        });
+    });
+});
